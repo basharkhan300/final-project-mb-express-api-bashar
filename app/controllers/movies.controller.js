97eceaf43d36ee9df6e248865355e1418369197a
Bashar <bashar.khan.17.1@mountblue.tech>
2021-10-25 21:48:35 +0530

    Retrieve all movies

---
+++ app/controllers/movies.controller.js
@@ -0,0 +1,22 @@
+const db = require("../models");
+const Movie = db.movies;
+const Op = db.Sequelize.Op;
+
+
+// Retrieve all Tutorials from the database.
+
+exports.findAll = (req, res) => {
+    const title = req.query.title;
+    var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+  
+    Movie.findAll({ where: condition })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving tutorials."
+        });
+      });
+  };
