7dca09d4f2e5a06519b6d471224bc0b062a23a02
Bashar <bashar.khan.17.1@mountblue.tech>
2021-10-26 15:10:05 +0530

    Get a movie with id function

--- app/controllers/movies.controller.js
+++ app/controllers/movies.controller.js
@@ -1,55 +1,71 @@
 const db = require("../models");
 const Movie = db.movies;
-const Op = db.Sequelize.Op;
-
 
 exports.create = (req, res) => {
   // Validate request
   if (!req.body.title) {
     res.status(400).send({
-      message: "Content can not be empty!"
+      message: "Content can not be empty!",
     });
     return;
   }
 
-  // Create a Tutorial
+  // Create a Movie
   const movie = {
-    Director: req.body.Director,
     title: req.body.title,
     description: req.body.description,
     production: req.body.production,
+    Director: req.body.Director,
     Genre: req.body.Genre,
     Ratings: req.body.Ratings,
   };
 
-  // Save Tutorial in the database
+  // Save Movie in the database
   Movie.create(movie)
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the Tutorial."
+          err.message || "Some error occurred while creating the Tutorial.",
+      });
+    });
+};
+
+// Retrieve all Movies from the database.
+
+exports.findAll = (req, res) => {
+  Movie.findAll()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials.",
       });
     });
 };
 
 
-// Retrieve all Tutorials from the database.
 
-exports.findAll = (req, res) => {
-    const title = req.query.title;
-    var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
-  
-    Movie.findAll({ where: condition })
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving tutorials."
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Movie.findByPk(id)
+    .then((data) => {
+      if (data) res.send(data);
+      else {
+        res.status(404).send({
+          message: `Cannot find Movie with id=${id}.`,
         });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: `Error retrieving Movie with Id=${id}.`,
       });
-  };
+    });
+};
+
