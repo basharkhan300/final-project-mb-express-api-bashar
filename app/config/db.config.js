133f3fa2f3ef2d2e53524a6754f1e60e284d9f45
Bashar <bashar.khan.17.1@mountblue.tech>
2021-10-26 11:02:06 +0530

    Details for database connection like Url

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,6 +2,7 @@ module.exports = {
     URL: 'postgres://ztabresg:Eo44wYEojdesSWBaUBb6pf94Wefe5tUs@fanny.db.elephantsql.com/ztabresg',
     HOST: "localhost",
     dialect: "postgres",
+    
   pool: {
     max: 5,
     min: 0,
