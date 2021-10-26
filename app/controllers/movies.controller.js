4c5d3937798a92dcb28e476b169375e3c53c0757
Bashar <bashar.khan.17.1@mountblue.tech>
2021-10-26 12:30:05 +0530

    create a new movie: post method

--- app/controllers/movies.controller.js
+++ app/controllers/movies.controller.js
@@ -3,6 +3,39 @@ const Movie = db.movies;
 const Op = db.Sequelize.Op;
 
 
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.title) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+
+  // Create a Tutorial
+  const movie = {
+    Director: req.body.Director,
+    title: req.body.title,
+    description: req.body.description,
+    production: req.body.production,
+    Genre: req.body.Genre,
+    Ratings: req.body.Ratings,
+  };
+
+  // Save Tutorial in the database
+  Movie.create(movie)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Tutorial."
+      });
+    });
+};
+
+
 // Retrieve all Tutorials from the database.
 
 exports.findAll = (req, res) => {
